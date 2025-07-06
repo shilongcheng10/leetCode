b8f49008a92406ce8f6bc3b8e118957f2c23e22d
shilongcheng <shilongcheng10@outlook.com>
2025-07-06 11:35:46 +0800

    leetCode 2.

---
+++ src/main/java/com/fengjunzi/algorithms/AddTwoNumbers.java
@@ -0,0 +1,85 @@
+package com.fengjunzi.algorithms;
+//给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+//
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。 
+//
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：l1 = [2,4,3], l2 = [5,6,4]
+//输出：[7,0,8]
+//解释：342 + 465 = 807.
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [0], l2 = [0]
+//输出：[0]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//输出：[8,9,9,9,0,0,0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 每个链表中的节点数在范围 [1, 100] 内 
+// 0 <= Node.val <= 9 
+// 题目数据保证列表表示的数字不含前导零 
+// 
+//
+// Related Topics 递归 链表 数学 👍 11357 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode p = head;
+        int carry =0;
+        while(l1 !=null || l2!=null){
+            int num1 = (l1 == null) ? 0 : l1.val;
+            int num2 = (l2 == null) ? 0 : l2.val;
+            int sum = num1 + num2 + carry;
+            int nodeVal = sum %10;
+            carry = sum/10;
+            ListNode cur = new ListNode(nodeVal);
+            p.next = cur;
+            p = cur;
+            if(l1 !=null){
+                l1 = l1.next;
+            }
+            if(l2 !=null){
+                l2 = l2.next;
+            }
+
+        }
+        if(carry >0 ){
+            p.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+
+
+     public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
