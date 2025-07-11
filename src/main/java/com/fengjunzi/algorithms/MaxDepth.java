804f6c9c4bfd4b015d8ed10a752131ac74c77c36
shilongcheng <shilongcheng10@outlook.com>
2025-07-11 11:09:01 +0800

    leetcode 104

---
+++ src/main/java/com/fengjunzi/algorithms/MaxDepth.java
@@ -0,0 +1,108 @@
+package com.fengjunzi.algorithms;//给定一个二叉树 root ，返回其最大深度。
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 10⁴] 区间内。 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1992 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaxDepth {
+    //深度优先
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+    }
+    //广度优先
+    public int maxDepthSolution2(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size >0){
+                TreeNode node = queue.poll();
+                if(node.left != null){
+                    queue.offer(node.left);
+                }
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
