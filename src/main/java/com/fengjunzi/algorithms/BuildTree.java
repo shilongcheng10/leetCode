5014465e947c0ae29784530854b7a83e66a79e57
shilongcheng <shilongcheng10@outlook.com>
2025-07-12 11:59:44 +0800

    leetcode 105

---
+++ src/main/java/com/fengjunzi/algorithms/BuildTree.java
@@ -0,0 +1,125 @@
+package com.fengjunzi.algorithms;//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+// 
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 2558 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BuildTree {
+    private Map<Integer, Integer> index;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        index = new HashMap<Integer, Integer>();
+        for(int  i = 0 ; i < n ; i++){
+            index.put(inorder[i], i);
+        }
+
+        return myBuildTree(preorder,inorder, 0, n-1, 0, n-1);
+
+    }
+
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if(preorder_left > preorder_right){
+            return null;
+        }
+        int preorder_root = preorder_left;
+        int inorder_root = index.get(preorder[preorder_root]);
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        int size_left_sub_tree = inorder_root - inorder_left;
+        root.left = myBuildTree(preorder, inorder, preorder_left+1, preorder_left + size_left_sub_tree,inorder_left, inorder_root-1);
+        root.right = myBuildTree(preorder, inorder, preorder_left+1+size_left_sub_tree, preorder_right, inorder_root+1, inorder_right);
+        return root;
+    }
+
+    public TreeNode buildTreeSolution2(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
