3aaad35db1fafe93e28f9d629952c69859831a99
shilongcheng <shilongcheng10@outlook.com>
2025-07-24 12:07:46 +0800

    leetcode 103

---
+++ src/main/java/com/fengjunzi/algorithms/ZigzagLevelOrder.java
@@ -0,0 +1,103 @@
+package com.fengjunzi.algorithms;
+//给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[20,9],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 974 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class ZigzagLevelOrder {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null){
+            return res;
+        }
+        int j =0;
+        Deque<TreeNode> nodeDeque = new LinkedList<TreeNode>();
+        nodeDeque.offer(root);
+        while(!nodeDeque.isEmpty()){
+            int n = nodeDeque.size();
+            List<Integer> levelRes = new ArrayList();
+            for(int i = 0; i < n;i++){
+                TreeNode node = nodeDeque.poll();
+                levelRes.add(node.val);
+                if(node.left != null){
+                    nodeDeque.add(node.left);
+                }
+                if(node.right != null){
+                    nodeDeque.add(node.right);
+                }
+            }
+
+            res.add(j % 2 == 0 ? levelRes : levelRes.reversed());
+            j++;
+        }
+        return res;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
