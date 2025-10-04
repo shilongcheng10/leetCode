2869569377a3fe8fa152a66d426ec801c77b458c
Shi, Longcheng <shilongcheng10@outlook.com>
2025-10-04 16:26:14 +0800

    leetcode 188 买卖股票的最佳时机IV 解法1： 三维数组动态规划 解法2: 动态规划 + 状态机

---
+++ src/main/java/com/fengjunzi/algorithms/maxProfitK.java
@@ -0,0 +1,80 @@
+package com.fengjunzi.algorithms;//给你一个整数数组 prices 和一个整数 k ，其中 prices[i] 是某支给定的股票在第 i 天的价格。
+//
+// 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。也就是说，你最多可以买 k 次，卖 k 次。 
+//
+// 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：k = 2, prices = [2,4,1]
+//输出：2
+//解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。 
+//
+// 示例 2： 
+//
+// 
+//输入：k = 2, prices = [3,2,6,5,0,3]
+//输出：7
+//解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+//     随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 
+//。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= k <= 100 
+// 1 <= prices.length <= 1000 
+// 0 <= prices[i] <= 1000 
+// 
+//
+// Related Topics 数组 动态规划 👍 1306 👎 0
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class maxProfitK {
+    //动态规划，三维数组
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n+1][k+1][2];
+        for(int j = 0; j<=k;j++){
+            dp[0][j][0] = 0;
+            dp[0][j][1] = Integer.MIN_VALUE;
+        }
+        for(int i =0; i <=n;i++){
+            dp[i][0][0] = 0;
+            dp[i][0][1] = Integer.MIN_VALUE;
+        }
+        for(int i =1; i <=n; i++){
+            for(int j =1; j <=k; j++){
+                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i-1]);
+                dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i-1]);
+            }
+        }
+        return dp[n][k][0];
+    }
+
+    // 动态规划 + 状态机
+    public int maxProfit2(int k, int[] prices) {
+        int n = prices.length;
+        if(n == 0) return 0;
+        int[] sell = new int[k+1];
+        int[] buy = new int[k+1];
+        Arrays.fill(buy, -prices[0]);
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=k;j++){
+                buy[j] = Math.max(buy[j], sell[j-1] - prices[i]);
+                sell[j] = Math.max(sell[j], buy[j] + prices[i]);
+            }
+        }
+        return sell[k];
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
