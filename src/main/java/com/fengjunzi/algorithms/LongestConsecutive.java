712f121d340eb2b20cd85432d1d1eb60a35b73f3
Shi, Longcheng <shilongcheng10@outlook.com>
2025-10-11 14:47:12 +0800

    leetcode 128 最长连续序列 解法1: hashset

---
+++ src/main/java/com/fengjunzi/algorithms/LongestConsecutive.java
@@ -0,0 +1,65 @@
+package com.fengjunzi.algorithms;//给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+//
+// 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [100,4,200,1,3,2]
+//输出：4
+//解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,3,7,2,5,8,4,6,0,1]
+//输出：9
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1,0,1,2]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// 
+//
+// Related Topics 并查集 数组 哈希表 👍 2646 👎 0
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int max = 0;
+        for (int num : nums) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentMax = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentMax++;
+                }
+                max = Math.max(max, currentMax);
+            }
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
