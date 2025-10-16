762698059940329dd1bcc4ccdb07588648359025
Shi, Longcheng <shilongcheng10@outlook.com>
2025-10-16 12:17:15 +0800

    leetcode 236 二叉树最近的公共祖先 解法： dfs

--- src/main/java/com/fengjunzi/algorithms/LowestCommonAncestor.java
+++ src/main/java/com/fengjunzi/algorithms/LowestCommonAncestor.java
@@ -55,15 +55,14 @@ package com.fengjunzi.algorithms;//给定一个二叉树, 找到该树中两个�
  */
 class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root.equals(p) || root.equals(q)){
+        if(root == null || root == p || root == q){
             return root;
         }
-        TreeNode left = lowestCommonAncestor(root.left, p,q);
-        TreeNode right =  lowestCommonAncestor(root.right, p, q);
-        if(left !=null && right != null){
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left != null && right != null){
             return root;
-        }
-        if(left !=null){
+        }else if(left != null){
             return left;
         }else{
             return right;
